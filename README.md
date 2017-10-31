c0934e9de200d4a978cb87ea4b896f5917537f8e
tylerharrold <tylerharrold@gmail.com>
2017-10-31 09:04:56 -0400

    Update readme

---
+++ README.md
@@ -0,0 +1,5 @@
+Outside Libraries:
+	This program utilizes the pandas and numpy libraries. These libraries are most easily installed alongside
+Anacnda, which can be installed at the following address:
+
+https://docs.anaconda.com/anaconda/install/
--- insight_testsuite/temp/src/cleaning_functions.py
+++ insight_testsuite/temp/src/cleaning_functions.py
@@ -1,18 +1,21 @@
 import numpy as np
 import math
 
+# helper function to ensure other_id is clean
 def clean_other_id(incoming_value):
     if not incoming_value:
         return ''
     else:
         return incoming_value
 
+# helper function to ensure that transaction_dt is a valid string
 def clean_transaction_dt(incoming_value):
     if not incoming_value:
         return ''
     else:
         return str(incoming_value)
 
+# helper function to ensure that transaction_amt is an integer value in our data
 def clean_transaction_amt(incoming_value):
     if not incoming_value:
         return np.nan
